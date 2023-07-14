4a0a2f6caf0d9ad8197806faa54066316b872f20
anartzdev <105705996+anartzdev@users.noreply.github.com>
2023-07-14 10:14:25 +0200

    feat: first commit with work to environment

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,22 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$, useSignal, useTask$ } from "@builder.io/qwik";
+import { server$, type DocumentHead, } from "@builder.io/qwik-city";
+
+export const serverFunction = server$(function () {
+  const envPrivateValue = this.env.get('DB_PRIVATE_KEY');
+  // `this` is the `RequestEvent` object
+  console.log(envPrivateValue);
+  return envPrivateValue || '';
+});
 
 export default component$(() => {
+  const env = useSignal('');
+  useTask$(async () => {
+    env.value = await serverFunction() ;
+    console.log(env.value);
+  })
   return (
     <>
-      <h1>Hi 👋</h1>
+      <h1>Hi { env.value }👋</h1>
       <p>
         Can't wait to see what you build with qwik!
         <br />
